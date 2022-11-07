e413ddc0995d6293080cfa21a621b12582f0644f
SergeyLubash <luba-sergei@yandex.ru>
2022-11-07 20:54:44 +0300

    HW_30

--- users/serializers.py
+++ users/serializers.py
@@ -29,6 +29,7 @@ class UserCreateSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
         user.set_password(user.password)
+        user.save()
 
         for loc in self._location:
             location, _ = Location.objects.get_or_create(name=loc)
